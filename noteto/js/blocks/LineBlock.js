cdee4b723ddeb5b40934f20916dc9d53db70554e
dynobo <dynobo@mailbox.org>
2021-03-06 22:48:10 +0100

    Refactor

--- noteto/js/blocks/LineBlock.js
+++ noteto/js/blocks/LineBlock.js
@@ -7,6 +7,9 @@ class LineBlock extends BaseBlock {
     this.blockOpts.opts.titleText.value = 'Notes';
   }
 
+  /**
+   * Delete all line from block.
+   */
   clearLines() {
     const lines = this.svg.querySelectorAll('.horizontal-line');
     lines.forEach((line) => {
@@ -14,32 +17,25 @@ class LineBlock extends BaseBlock {
     });
   }
 
+  /**
+   * Add line to block and style them.
+   */
   renderLines() {
-    const borderMargin = this.globalOpts.get('borderMargin');
-    const borderStrokeWidth = this.globalOpts.get('borderStrokeWidth');
-    const titlePadding = this.globalOpts.get('titlePadding');
-    const titleFontSize = this.globalOpts.get('titleFontSize');
     const lineDistance = this.globalOpts.get('lineDistance');
     const lineStrokeWidth = this.globalOpts.get('lineStrokeWidth');
     const lineStrokeColor = this.globalOpts.get('lineStrokeColor');
 
     // Number of lines to draw
-    const lineCount = this.height / lineDistance;
-
-    // Offset depends on title visibility
-    let yOffset = borderMargin;
-    if (this.blockOpts.get('titleText').length > 0) {
-      yOffset += titleFontSize + titlePadding * 2;
-    }
+    const lineCount = this.innerHeight / lineDistance;
 
     // Draw lines
     for (let i = 1; i <= lineCount; i += 1) {
       // Use filled rect as line, because line doesn't work well with mask
       const lineRect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
       lineRect.setAttribute('class', 'horizontal-line');
-      lineRect.setAttribute('x', borderMargin + borderStrokeWidth);
-      lineRect.setAttribute('y', lineDistance * i + yOffset);
-      lineRect.setAttribute('width', this.width - borderMargin * 2 - borderStrokeWidth * 2);
+      lineRect.setAttribute('x', this.xOffset);
+      lineRect.setAttribute('y', lineDistance * i + this.yOffset);
+      lineRect.setAttribute('width', this.innerWidth);
       lineRect.setAttribute('height', lineStrokeWidth);
       lineRect.setAttribute('fill', lineStrokeColor);
       lineRect.setAttribute('mask', `url(#${this.id}_clip)`);
@@ -47,6 +43,9 @@ class LineBlock extends BaseBlock {
     }
   }
 
+  /**
+   * Hook that gets execute after rendering of the parents class elements
+   */
   onRender() {
     this.clearLines();
     this.renderLines();
